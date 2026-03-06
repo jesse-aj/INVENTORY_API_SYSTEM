ddb89096218acee4a640ae747c56c296cccd7254
jesse-aj <appiahjesse123@gmail.com>
2026-03-06 23:33:43 +0000

    added and configured JWT in settings and urls

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "inventory",
     "users",
+    "simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
