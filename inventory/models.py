2e51820c761c2fe8423ecc5fae412f2713998840
jesse-aj <appiahjesse123@gmail.com>
2026-03-06 02:51:58 +0000

    Add InventoryItem and InventoryChangeLog models

--- inventory/models.py
+++ inventory/models.py
@@ -17,4 +17,18 @@ class InventoryItem(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+    def __str__(self):
+        return self.name
+  
+class InventoryChangeLog(models.Model):
+    item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)
+    changed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    old_quantity = models.IntegerField()
+    new_quantity = models.IntegerField()
+    changed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.item} - {self.changed_at}"
+
+
