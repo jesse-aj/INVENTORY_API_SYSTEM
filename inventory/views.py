5ab3de29fc48df303eedc9838636e2c82eadfb05
jesse-aj <appiahjesse123@gmail.com>
2026-03-06 03:31:43 +0000

    Created views for Inventory and InventoryChangeLog

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import InventoryItem, InventoryChangeLog
+from .serializers import InventorySerializer, InventoryChangeLogSerializer
 
-# Create your views here.
+
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventorySerializer
+
+
+class InventoryChangeLogViewSet(viewsets.ModelViewSet):
+    queryset = InventoryChangeLog.objects.all()
+    serializer_class = InventoryChangeLogSerializer
