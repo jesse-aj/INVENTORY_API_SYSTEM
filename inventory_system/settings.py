6e82c91195942f33e912890651a8d868fde7b66d
jesse-aj <appiahjesse123@gmail.com>
2026-03-07 09:50:13 +0000

    Set authentication globally in settings.py so everything is protected by default

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -87,6 +87,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+
 }
 
 # Password validation
