c25f566d4907b28b32c7067b13f0a74d87684be8
jesse-aj <appiahjesse123@gmail.com>
2026-03-07 10:54:13 +0000

    add permission to InventoryViewSet

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,8 @@ from rest_framework import routers
 from .views import InventoryChangeLogViewSet, InventoryViewSet
 from users.views import UserViewSet
 
+
+
 router = routers.DefaultRouter()
 router.register(r'inventory', InventoryViewSet)
 router.register(r'changelog', InventoryChangeLogViewSet)
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from .models import InventoryItem, InventoryChangeLog
 from .serializers import InventorySerializer, InventoryChangeLogSerializer
+from .permissions import IsOwner
+from rest_framework.permissions import IsAuthenticated
+
+
 
 
 class InventoryViewSet(viewsets.ModelViewSet):
@@ -11,3 +15,9 @@ class InventoryViewSet(viewsets.ModelViewSet):
 class InventoryChangeLogViewSet(viewsets.ModelViewSet):
     queryset = InventoryChangeLog.objects.all()
     serializer_class = InventoryChangeLogSerializer
+
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+           return [IsAuthenticated()]
+        return [IsAuthenticated(),  IsOwner()]
