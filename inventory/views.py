09199bdbb6783b4c11414309c0076aaf2b785c84
jesse-aj <appiahjesse123@gmail.com>
2026-03-07 10:58:04 +0000

    added permissions each both to all viwset

--- inventory/views.py
+++ inventory/views.py
@@ -11,11 +11,19 @@ class InventoryViewSet(viewsets.ModelViewSet):
     queryset = InventoryItem.objects.all()
     serializer_class = InventorySerializer
 
+# If action is list or retrieve (GET)  IsAuthenticated 
+# If action is create, update or destroy (POST, PUT, DELETE)
+# IsAuthenticated AND IsOwner
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+           return [IsAuthenticated()]
+        return [IsAuthenticated(),  IsOwner()]
+
 
 class InventoryChangeLogViewSet(viewsets.ModelViewSet):
     queryset = InventoryChangeLog.objects.all()
     serializer_class = InventoryChangeLogSerializer
-
 
     def get_permissions(self):
         if self.action in ["list", "retrieve"]:
